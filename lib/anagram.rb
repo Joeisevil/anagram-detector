bc55636ac57fe6e7327988da0995f6fedaeebd53
Joe Downes-Berry <j.d.berry26@gmail.com>
2021-01-20 15:49:34 -0500

    commit tree 198cb38ce8cd7ecbd54a784d259c9da48ff33f68

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :anagram
+
+    def initialize(anagram)
+      @anagram = anagram
+    end
+  
+    def match(words)
+      words.select do |word|
+        anagram?(word)
+      end
+    end
+  
+    def anagram?(word)
+      word.chars.sort == @anagram.chars.sort
+    end
+end
